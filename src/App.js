e66b6114da85ecf40f96b8593951bf7e3e1d02fc
Nattamai Gopinath <r.nattamai.gopinath@accenture.com>
2024-03-25 17:12:54 +0530

    Completed file upload in server side

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
-function App() {
-  return <h1>Hello World</h1>
-}
+import { useEffect, useState } from "react";
 
-export default App;
+export default function App() {
+  const [data, setData] = useState("")
+  const [failedTransactions, setFailedTransactions] = useState("")
+  const [error, setError] = useState("")
+  const [file, setFile] = useState("")
+  const [fileType, setFileType] = useState("")
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     if (data) {
+  //       const response = await fetch("http://localhost:5000/upload", {
+  //         method: "POST",
+  //         body: JSON.stringify({
+  //           data
+  //         }),
+  //       })
+  //       const resData = await response.json()
+  //       setFailedTransactions(resData)
+  //     }
+  //   }
+  //   getData()
+  // }, [data])
+
+  useEffect(() => {
+    const postData = async() => {
+      if(file) {
+        const response = await fetch("http://localhost:5000/upload", {
+          method: "POST",
+          body: file,
+        })
+        const resData = await response.json()
+        // setFailedTransactions(resData)
+      }
+    }
+    postData()
+  }, [file])
+
+  const changeHandler = (event) => {
+    let formData = new FormData();
+    formData.append("file", event.target.files[0]);
+    setFile(formData)
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <input
+          type="file"
+          name="file"
+          accept=".csv,.xml"
+          onChange={changeHandler}
+        />
+      </div>
+    </div>
+  );
+}
